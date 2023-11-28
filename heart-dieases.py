e18732e01935efb61fdb35cabf5413cdd5eb1b74
Tanusree Sen <30462968+Tanushree77@users.noreply.github.com>
2023-11-28 20:22:24 +0600

    Add files via upload

---
+++ heart-dieases.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import pandas as pd
+from tensorflow import keras
+
+# dataset load 
+
+file_url = "http://storage.googleapis.com/download.tensorflow.org/data/heart.csv"
+dataframe = pd.read_csv(file_url)
+
+dataframe.shape
+
+print(dataframe.head())
+
+val_dataframe = dataframe.sample(frac=0.2, random_state=1337)
+train_dataframe = dataframe.drop(val_dataframe.index)
+
+print(
+    "Using %d samples for training and %d for validation"
+    % (len(train_dataframe), len(val_dataframe))
+)
+
+# convert pandas into tensoflow dataset 
+
+def dataframe_to_dataset(dataframe):
+    dataframe = dataframe.copy()
+    labels = dataframe.pop("target")
+    ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+    ds = ds.shuffle(buffer_size=len(dataframe))
+    return ds
+
+
+train_ds = dataframe_to_dataset(train_dataframe)
+val_ds = dataframe_to_dataset(val_dataframe)
